75f93edc9762443662ac2906fa45ae1c281de85a
Odin <odin@boydsbar.xyz>
2024-01-04 07:48:02 -0600

    added additional sudo lines.

--- ssh_lockdown_local.sh
+++ ssh_lockdown_local.sh
@@ -20,18 +20,20 @@ sleep 5
 ssh-keygen -t rsa -b 2048 -f ~/.ssh/bastion_key
 
 
+#### [SECTION] PAM Configurations ####
+
 
 ##### Adds requires authentication items to SSH
 sudo cp /etc/pam.d/sshd /etc/pam.d/sshd.bak
-echo "auth required pam_google_authenticator.so" >> /etc/pam.d/sshd 
-echo "auth required pam_permit.so" >> /etc/pam.d/sshd
+sudo echo "auth required pam_google_authenticator.so" >> /etc/pam.d/sshd 
+sudo echo "auth required pam_permit.so" >> /etc/pam.d/sshd
 
 ##### Changes SSH acceptance methods to be PublicKey, MFA
 sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-sed -i 's/^ChallengeResponseAuthentication.*/ChallengeResponseAuthentication yes' /etc/ssh/sshd_config
-echo "AuthenticationMethods publickey,password publickey,keyboard-interactive" >> /etc/ssh/sshd_config
+sudo sed -i 's/^ChallengeResponseAuthentication.*/ChallengeResponseAuthentication yes' /etc/ssh/sshd_config
+sudo echo "AuthenticationMethods publickey,password publickey,keyboard-interactive" >> /etc/ssh/sshd_config
 #### Stops password only logins
-sed -i 's/^@include common-auth/#&/' /etc/pam.d/sshd
+sudo sed -i 's/^@include common-auth/#&/' /etc/pam.d/sshd
 
 service ssh restart
 
@@ -63,28 +65,28 @@ function apply_interactively {
     # Modify PermitRootLogin
     read -p "PermitRootLogin (yes/no): " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^PermitRootLogin.*/PermitRootLogin $new_value/" /etc/ssh/sshd_config
+        sudo sed -i "s/^PermitRootLogin.*/PermitRootLogin $new_value/" /etc/ssh/sshd_config
         confirm_change
     fi
 
     # Modify Protocol
     read -p "Protocol (2): " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^Protocol.*/Protocol $new_value/" /etc/ssh/sshd_config
+        sudo sed -i "s/^Protocol.*/Protocol $new_value/" /etc/ssh/sshd_config
         confirm_change
     fi
 
         # Modify MaxAuthTries
     read -p "MaxAuthTries (3): " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^MaxAuthTries.*/MaxAuthTries $new_value/" /etc/ssh/sshd_config
+        sudo sed -i "s/^MaxAuthTries.*/MaxAuthTries $new_value/" /etc/ssh/sshd_config
         confirm_change
     fi
 
     # Modify ClientAliveInterval
     read -p "ClientAliveInterval (300): " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^ClientAliveInterval.*/ClientAliveInterval $new_value/" /etc/ssh/sshd_config
+        sudo sed -i "s/^ClientAliveInterval.*/ClientAliveInterval $new_value/" /etc/ssh/sshd_config
         confirm_change
     fi
 
@@ -92,7 +94,7 @@ function apply_interactively {
     echo "Sets the number of concurrent logins"
     read -p "ClientAliveCountMax (2): " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^ClientAliveCountMax.*/ClientAliveCountMax $new_value/" /etc/ssh/sshd_config
+        sudo sed -i "s/^ClientAliveCountMax.*/ClientAliveCountMax $new_value/" /etc/ssh/sshd_config
         confirm_change
     fi
 
@@ -100,7 +102,7 @@ function apply_interactively {
     echo "Without setting this, logins can attempt to login without a password"
     read -p "PermitEmptyPasswords (no): " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^PermitEmptyPasswords.*/PermitEmptyPasswords $new_value/" /etc/ssh/sshd_config
+        sudo sed -i "s/^PermitEmptyPasswords.*/PermitEmptyPasswords $new_value/" /etc/ssh/sshd_config
         confirm_change
     fi
 
@@ -108,7 +110,7 @@ function apply_interactively {
     echo "Requires publickey would recommend this, but we achieve a different result with PAM."
     read -p "PubkeyAuthentication (yes): " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^PubkeyAuthentication.*/PubkeyAuthentication $new_value/" /etc/ssh/sshd_config
+        sudo sed -i "s/^PubkeyAuthentication.*/PubkeyAuthentication $new_value/" /etc/ssh/sshd_config
         confirm_change
     fi
 
@@ -116,7 +118,7 @@ function apply_interactively {
     echo "Sets password authentication, but we achieve a different result with PAM 'AuthenticationMethods'."
     read -p "PasswordAuthentication (no): " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^PasswordAuthentication.*/PasswordAuthentication $new_value/" /etc/ssh/sshd_config
+        sudo sed -i "s/^PasswordAuthentication.*/PasswordAuthentication $new_value/" /etc/ssh/sshd_config
         confirm_change
     fi
 
@@ -124,14 +126,14 @@ function apply_interactively {
     echo "Restricts SSH to ONLY listed user, specifically the one user created above."
     read -p "AllowUsers (username): " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^AllowUsers.*/AllowUsers $USERNAME/" /etc/ssh/sshd_config
+        sudo sed -i "s/^AllowUsers.*/AllowUsers $USERNAME/" /etc/ssh/sshd_config
         confirm_change
     fi
 
     # Modify AllowGroups
     read -p "Script does not currently create a special Group, but you can set one if you want. AllowGroups (groupname) : " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^AllowGroups.*/AllowGroups $new_value/" /etc/ssh/sshd_config
+        sudo sed -i "s/^AllowGroups.*/AllowGroups $new_value/" /etc/ssh/sshd_config
         confirm_change
     fi
 
@@ -140,7 +142,7 @@ function apply_interactively {
     echo "However, automated bots do just hammer port 22 so changing it to something different isn't a bad idea."
     read -p "Port (2222): " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^Port.*/Port $new_value/" /etc/ssh/sshd_config
+        sudo sed -i "s/^Port.*/Port $new_value/" /etc/ssh/sshd_config
         confirm_change
     fi
 
@@ -148,7 +150,7 @@ function apply_interactively {
     echo "Stops remote applications running via SSH connections"
     read -p "X11Forwarding (no): " new_value
     if [ -n "$new_value" ]; then
-        sed -i "s/^X11Forwarding.*/X11Forwarding $new_value/" /etc/ssh/sshd_config
+        sudo sed -i "s/^X11Forwarding.*/X11Forwarding $new_value/" /etc/ssh/sshd_config
         confirm_change
     fi
 
@@ -163,28 +165,28 @@ function apply_interactively {
 function apply_automatically {
     # Edit sshd_config with desired settings using sed
     # You may need to adjust these settings based on your requirements
-    sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
+    sudo sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
     # Sets to Protocol 2 rather than 1 which has known vulnerabilities.
-    sed -i 's/^Protocol.*/Protocol 2/' /etc/ssh/sshd_config
+    sudo sed -i 's/^Protocol.*/Protocol 2/' /etc/ssh/sshd_config
     # Sets the number of retries if the user fails to authenticate within the allowed number of attempts, the connection will be terminated.
-    sed -i 's/^MaxAuthTries.*/MaxAuthTries 3/' /etc/ssh/sshd_config
+    sudo sed -i 's/^MaxAuthTries.*/MaxAuthTries 3/' /etc/ssh/sshd_config
     # Sets the length of a connection without action
-    sed -i 's/^ClientAliveInterval.*/ClientAliveInterval 300/' /etc/ssh/sshd_config
+    sudo sed -i 's/^ClientAliveInterval.*/ClientAliveInterval 300/' /etc/ssh/sshd_config
     # Sets the number of concurrent logins
-    sed -i 's/^ClientAliveCountMax.*/ClientAliveCountMax 2/' /etc/ssh/sshd_config
-    sed -i 's/^PermitEmptyPasswords.*/PermitEmptyPasswords no/' /etc/ssh/sshd_config
+    sudo sed -i 's/^ClientAliveCountMax.*/ClientAliveCountMax 2/' /etc/ssh/sshd_config
+    sudo sed -i 's/^PermitEmptyPasswords.*/PermitEmptyPasswords no/' /etc/ssh/sshd_config
     # Requires publickey would recommend this, but we achieve a different result with PAM.
-    sed -i 's/^PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+    sudo sed -i 's/^PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
     # Sets password authentication, but we achieve a different result with PAM "AuthenticationMethods".
-    sed -i 's/^PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
-    sed -i "s/^AllowUsers.*/AllowUsers $USERNAME/" /etc/ssh/sshd_config
+    sudo sed -i 's/^PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
+    sudo sed -i "s/^AllowUsers.*/AllowUsers $USERNAME/" /etc/ssh/sshd_config
     # Use this if you want to create a group that is allowed rather than the individual user like above
     # sed -i 's/^AllowGroups.*/AllowGroups groupname/' /etc/ssh/sshd_config
     # Changes the SSH Port. Generally, I am against this as it really doesn't serve a purpose sense a targeted scan would still be able to find OpenSSH.
     # However, automated bots do just hammer port 22 so changing it to something different isn't a bad idea.
-    sed -i 's/^Port.*/Port 30022/' /etc/ssh/sshd_config  # Change the port to a non-standard value
+    sudo sed -i 's/^Port.*/Port 30022/' /etc/ssh/sshd_config  # Change the port to a non-standard value
     # Stops remote applications running via SSH connections
-    sed -i 's/^X11Forwarding.*/X11Forwarding no/' /etc/ssh/sshd_config
+    sudo sed -i 's/^X11Forwarding.*/X11Forwarding no/' /etc/ssh/sshd_config
 
 
     # Restart SSH service to apply changes
