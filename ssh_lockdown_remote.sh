512b7b9087356f73969836b28a9cd0707209e209
Odin <odin@boydsbar.xyz>
2024-01-06 19:29:56 -0600

    Spelling correction

--- ssh_lockdown_remote.sh
+++ ssh_lockdown_remote.sh
@@ -3,31 +3,47 @@
 #### Backup the original sshd_config file
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 
+USERNAME=$(logname)
+
+
+function check_and_modify_option {
+  OPTION="$1"
+  VALUE="$2"
+  grep -i "^$OPTION" /etc/ssh/sshd_config > /dev/null
+  if [ $? -eq 0 ]; then
+    sed -i "s/^$OPTION .*/$OPTION $VALUE/" /etc/ssh/sshd_config
+  else
+    echo "$OPTION $VALUE" >> /etc/ssh/sshd_config
+  fi
+}
+
 #### You may need to adjust these settings based on your requirements
-### Ensure the "ROOT" cannot SSH in
-sed -i 's/^PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config
-### Change to SSH Protocol 2
-sed -i 's/^Protocol.*/Protocol 2/' /etc/ssh/sshd_config
-### Limits SSH Attempts
-sed -i 's/^MaxAuthTries.*/MaxAuthTries 3/' /etc/ssh/sshd_config
-### Limits SSH timeout 
-sed -i 's/^ClientAliveInterval.*/ClientAliveInterval 300/' /etc/ssh/sshd_config
-sed -i 's/^ClientAliveCountMax.*/ClientAliveCountMax 2/' /etc/ssh/sshd_config
-### Stops empty password attempts
-sed -i 's/^PermitEmptyPasswords.*/PermitEmptyPasswords no/' /etc/ssh/sshd_config
-### Requires publickey would recommend this, but we achieve a different result with PAM.
-# sed -i 's/^PubkeyAuthentication.*/PubkeyAuthentication yes/' /etc/ssh/sshd_config
-### Sets password authentication, but we achieve a different result with PAM "AuthenticationMethods".
-# sed -i 's/^PasswordAuthentication.*/PasswordAuthentication yes/' /etc/ssh/sshd_config
-### Restricts SSH to ONLY one user, the user created above
-sed -i "s/^AllowUsers.*/AllowUsers $USERNAME/" /etc/ssh/sshd_config
-### Use this if you want to create a group that is allowed rather than the individual user like above.
-# sed -i 's/^AllowGroups.*/AllowGroups groupname/' /etc/ssh/sshd_config
-### Changes the SSH Port. Generally, I am against this as it really doesn't serve a purpose sense a targeted scan would still be able to find OpenSSH.
-### However, automated bots do just hammer port 22 so changing it to something different isn't a bad idea.
-# sed -i 's/^Port.*/Port 30022/' /etc/ssh/sshd_config  # Change the port to a non-standard value
-### Stops remote applications running via SSH connections
-sed -i 's/^X11Forwarding.*/X11Forwarding no/' /etc/ssh/sshd_config
+function apply_automatically {
+    # Edit sshd_config with desired settings using sed
+    # You may need to adjust these settings based on your requirements
+    check_and_modify_option "PermitRootLogin" "no"
+    # Sets the number of retries if the user fails to authenticate within the allowed number of attempts, the connection will be terminated.
+    check_and_modify_option "MaxAuthTries" "3"
+    # Sets the length of a connection without action
+    check_and_modify_option "ClientAliveInterval" "300"
+    # Sets the number of concurrent logins
+    check_and_modify_option "ClientAliveCountMax" "2"
+    # Sets no empty passwords
+    check_and_modify_option "PermitEmptyPasswords" "no"
+    # Requires publickey would recommend this, but we achieve a different result with PAM.
+    check_and_modify_option "PubkeyAuthentication" "yes"
+    # Sets password authentication, but we achieve a different result with PAM "AuthenticationMethods".
+    check_and_modify_option "PasswordAuthentication" "no"
+    # Allow a specific user to access SSH
+    check_and_modify_option "AllowUsers" $USERNAME
+    # Use this if you want to create a group that is allowed rather/with than the individual user like above. Commenting out since it is not set by the script.
+    # check_and_modify_option AllowGroups $groupname
+    # Changes the SSH Port. Generally, I am against this as it really doesn't serve a purpose sense a targeted scan would still be able to find OpenSSH.
+    # However, automated bots do just hammer port 22 so changing it to something different isn't a bad idea.
+    check_and_modify_option "Port" "4022"
+    # Stops remote applications running via SSH connections
+    check_and_modify_option "X11Forwarding" "no"
+}
 
 ### Restart SSH service to apply changes
 systemctl restart sshd
